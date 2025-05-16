0498f15124c40d4d587f63fa864f8d982f18eff1
jeremiahatwork <jeremiah.joseph@graymatter.co.in>
2025-05-16 16:13:19 +0530

    chore: tailwind cleanup

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,7 @@
 const { getDefaultConfig } = require('@expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
 
 const defaultConfig = getDefaultConfig(__dirname);
 defaultConfig.resolver.sourceExts.push('cjs');
 defaultConfig.resolver.unstable_enablePackageExports = false;
 
-module.exports = withNativeWind(defaultConfig, { input: './global.css' });
+module.exports = defaultConfig;
